19b9991b643560f245b836373a2acce668c428a1
Eduardo Almeida Horta <eduhorta@hotmail.com>
2023-03-12 09:09:58 -0300

    search input style

--- src/App.tsx
+++ src/App.tsx
@@ -76,14 +76,29 @@ function App() {
         backgroundSize: 'cover',
       }}
     >
-      <div className="search">
-        <input
-          type="text"
-          value={location}
-          onChange={(event) => setLocation(event.target.value)}
-          onKeyDown={searchLocation}
-          placeholder="Enter Location"
-        ></input>
+      <div className="search flex justify-between py-3 px-6">
+        <div className="relative flex items-center text-gray-400 focus-within:text-gray-600">
+          <svg
+            className="pointer-events-none absolute ml-3 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="m19.6 21l-6.3-6.3q-.75.6-1.725.95Q10.6 16 9.5 16q-2.725 0-4.612-1.887Q3 12.225 3 9.5q0-2.725 1.888-4.613Q6.775 3 9.5 3t4.613 1.887Q16 6.775 16 9.5q0 1.1-.35 2.075q-.35.975-.95 1.725l6.3 6.3ZM9.5 14q1.875 0 3.188-1.312Q14 11.375 14 9.5q0-1.875-1.312-3.188Q11.375 5 9.5 5Q7.625 5 6.312 6.312Q5 7.625 5 9.5q0 1.875 1.312 3.188Q7.625 14 9.5 14Z"
+            />
+          </svg>
+          <input
+            className="rounded-2xl py-2 pr-3 pl-10 font-semibold text-black placeholder-gray-500 opacity-50 ring-2 ring-gray-300 focus:ring-2 focus:ring-gray-500"
+            type="text"
+            value={location}
+            onChange={(event) => setLocation(event.target.value)}
+            onKeyDown={searchLocation}
+            placeholder="Enter Location"
+          ></input>
+        </div>
       </div>
       <div className="container flex max-w-[25%] space-x-8 space-y-4 rounded-lg bg-gray-800 bg-opacity-40">
         <div className="top">
